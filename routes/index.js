c462a0bcc9fa5df296c83b268ad2cffb7f6a6e2c
Vincent Iroleh <irolehiroleh@gmail.com>
2020-06-08 14:30:29 +0100

    chore(build API):add graduates API

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const graduates = require('../_data/index');
 
-router.get('/', (req, res) => {
+router.get('/api', (req, res) => {
     res.status(200).send(graduates);
 })
 
