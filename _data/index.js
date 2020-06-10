8c4d5f9e4a3f79e65356aceb4a6a0a2802691ace
Vincent Iroleh <irolehiroleh@gmail.com>
2020-06-10 12:13:37 +0100

    update graduates details

--- _data/index.js
+++ _data/index.js
@@ -4,6 +4,7 @@ const graduates = {
             name: 'Blessing Anyanwu',
             img: 'https://bit.ly/3dK7e60',
             stack: 'FullStack',
+            twitter: 'https://twitter.com/blexnmirian',
             github: 'https://github.com/blexnmirian',
             linkedin: 'https://www.linkedin.com/in/blessing-anyanwu-2110b21a4'
         },
@@ -11,6 +12,7 @@ const graduates = {
             name: 'Victor Obumneme',
             img: 'https://bit.ly/2UjUA68',
             stack: 'FullStack',
+            twitter: 'https://twitter.com/VictorObumneme',
             github: 'https://github.com/victorobumneme',
             linkedin: 'https://www.linkedin.com/in/victorobumneme'
         },
@@ -18,6 +20,7 @@ const graduates = {
             name: 'Chinonso Ogbonna',
             img: 'https://bit.ly/3f1snIZ',
             stack: 'FullStack',
+            twitter: 'https://twitter.com/Ogbonnaubadire',
             github: 'https://github.com/ogbonnachinonso',
             linkedin: 'https://www.linkedin.com/in/chinonso-ubadire-ogbonna-31a0b11a4'
         },
