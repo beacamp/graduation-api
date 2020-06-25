30c3c3275a4508ecad1921ddf0396421b36e59b1
Vincent Iroleh <irolehiroleh@gmail.com>
2020-06-25 17:48:11 +0100

    chore(fix changes) made some few changes to the project

---
+++ _data/projects.js
@@ -0,0 +1,22 @@
+const projects = {
+    classOf2020: [
+        {
+            name: 'Project Name',
+            description: 'Project Description',
+            link: 'Project Link',
+        },
+        {
+            name: 'Project Name',
+            description: 'Project Description',
+            link: 'Project Link',
+        },
+        {
+            name: 'Project Name',
+            description: 'Project Description',
+            link: 'Project Link',
+        },
+    ]
+}
+
+
+module.exports = projects;
--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,18 @@
 const express = require('express');
 const router = express.Router();
-const graduates = require('../_data/index');
+const graduates = require('../_data/graduates');
+const projects = require('../_data/projects');
 
-router.get('/api', (req, res) => {
+router.get('/api/graduates', (req, res) => {
     res.status(200).send(graduates);
 })
 
+router.get('/api/projects', (req, res) => {
+    res.status(200).send(projects)
+})
+
+router.get('*', (req, res) => {
+    res.status(400).send(`<div style="color:red;"> 404: oh noes, there's nothing in here the page you're trying to reach does not exist <br> 🙄 Check the address or <a href="https://twitter.com/IrolehVincent">report an error to the developer</a> </div>`);
+})
 
 module.exports = router;
